6d8094a3bfd9a72ac2ac251576840aa9b0046031
Farendy Naufal <farendy.asyikin@students.amikom.ac.id>
2021-08-05 00:00:08 +0700

    Part 2

--- app/src/main/java/com/example/jrmpkalkulator/MainActivity.java
+++ app/src/main/java/com/example/jrmpkalkulator/MainActivity.java
@@ -2,8 +2,15 @@ package com.example.jrmpkalkulator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText et_1, et_2;
+    Button btn_tambah, btn_kali, btn_bagi, btn_kurang;
+    TextView tv_hasil;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
